b2b15aafe0651df592836d0fef20a7a142e963ff
Er-shuang <1343109389@qq.com>
2025-04-02 15:02:25 +0800

    Add the LED.py with some comments

--- LED.py
+++ LED.py
@@ -1,10 +1,26 @@
+# Author: Nie Yujin
+# Date: 2/4/25
+# Some code to switch the LED on, using the RPi.GPIO library
+
+# using RPi.GPIO library
 import RPi.GPIO as GPIO
+# import time library so that pause the script later on
 import time
+
+# Tell the program which convention is to be used
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
+
+# trun on the LED
 print("LED on")
-GPIO.output(18,GPIO.HIGH)
+# provide power of 3.3V
+GPIO.output(18,GPIO.HIGH) # red LED
+
+# Pause the program for 1 second
 time.sleep(1)
+
+# turn off the LED
 print("LED off")
+# do not supply any power
 GPIO.output(18,GPIO.LOW)
